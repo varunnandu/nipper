e0fea845b560e134e3f340b34b022add17468c8e
varunnandu1992 <varunnandu1992@ccs.neu.edu>
2018-02-22 21:44:58 -0500

    heroku configs

--- app.js
+++ app.js
@@ -3,8 +3,17 @@ module.exports = function (app) {
     var niperModel = require('./models/nipper.model.server');
     var model = {niperModel: niperModel};
     require('./services/nipper.service')(app, model);
-
     var connectionString = 'mongodb://127.0.0.1:27017/nipper';
+
+    if(process.env.MLAB_USERNAME) {
+        connectionString = process.env.MLAB_USERNAME + ":" +
+            process.env.MLAB_PASSWORD + "@" +
+            process.env.MLAB_HOST + ':' +
+            process.env.MLAB_PORT + '/' +
+            process.env.MLAB_APP_NAME;
+    }
+
+
     var mongoose = require("mongoose");
     mongoose.connect(connectionString);
 
